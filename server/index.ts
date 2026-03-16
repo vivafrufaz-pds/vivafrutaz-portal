6aed7cc4676cd8555b48f50335d85fe33cfdea6a
vivafrufaz-pds <frutazviva@gmail.com>
2026-03-16 00:58:00 -0300

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -83,7 +83,7 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  await registerRoutes(httpServer, app);
+  // await registerRoutes(httpServer, app);
 
   app.use((err: any, _req: Request, res: Response, next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
